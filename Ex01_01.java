0bf5912116fcaa264ae020c82e3fadcc9c191ff0
Jungbae99 <116333199+Jungbae99@users.noreply.github.com>
2023-04-17 16:22:22 +0900

    Update Ex01_01.java

--- Ex01_01.java
+++ Ex01_01.java
@@ -1,20 +1,18 @@
-package practice;
+package result;
 
 import java.util.Scanner;
 
-class Try01{
-	public int[] solution(int a) {
-		int [] answer = new int[a];
-		for(int i = 0; i < a; ++i) answer[i] = i+1;
-		return answer;
-	}
-}
-
-public class Ex01_01 {
+class Ex01_01{
 	public static void main (String[] args) {
-		Try01 T = new Try01();
-		Scanner in = new Scanner(System.in);
-		int a = in.nextInt();
-		for(int b : T.solution(a)) System.out.print(b + " ");
+		Scanner input = new Scanner(System.in);
+		String s = input.next();
+		char c = input.next().toUpperCase().charAt(0); 
+		int sum = 0;
+		for(int i = 0; i < s.length(); ++i) {
+			char a = s.toUpperCase().charAt(i);
+			if(c == a)
+				++sum;
+		}
+		System.out.println(sum);
 	}
-}
+	
